53b1fe6840b928d0a2e09212253910d9a25ad6a5
Pratik Sah <shivam3448@gmail.com>
2021-02-06 16:40:18 +0530

    Added database link

--- util/database.js
+++ util/database.js
@@ -1,8 +1,12 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("formdata", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// const sequelize = new Sequelize("formdata", "root", "", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
+
+const sequelize = new Sequelize("mysql://ba00d2f2fba91d:ccb5e34a@eu-cdbr-west-03.cleardb.net/heroku_428435f2eceecbb?reconnect=true", {
+  dialect: "mysql"
+})
 
 module.exports = sequelize;
